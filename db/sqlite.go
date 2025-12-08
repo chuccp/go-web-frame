33602df47f3e486669f6cc72b1e6de1b523e07ad
tomcat <cooge123@gmail.com>
2025-12-08 13:50:35 +0800

    Update sqlite.go

--- db/sqlite.go
+++ db/sqlite.go
@@ -1,1 +1,13 @@
 package db
+
+import (
+	"github.com/chuccp/go-web-frame/config"
+	"gorm.io/gorm"
+)
+
+type SQLite struct {
+}
+
+func (ms *SQLite) Connection(cfg *config.Config) (db *gorm.DB, err error) {
+	return nil, nil
+}

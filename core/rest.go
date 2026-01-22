e3afdb5ac1b0a3a0affa91f856366ae0988fb4da
tomcat <cooge123@gmail.com>
2026-01-22 23:45:42 +0800

    Update rest.go

--- core/rest.go
+++ core/rest.go
@@ -10,6 +10,7 @@ type RestGroup struct {
 	converter    IConverter
 	filters      []IFilter
 	serverConfig *web.ServerConfig
+	host         []string
 }
 
 func (rg *RestGroup) Port() int {
@@ -20,6 +21,10 @@ func (rg *RestGroup) AddRest(rest ...IRest) *RestGroup {
 	return rg
 }
 
+func (rg *RestGroup) Host(host ...string) *RestGroup {
+	rg.host = host
+	return rg
+}
 func (rg *RestGroup) AddFilter(filter ...IFilter) *RestGroup {
 	rg.filters = append(rg.filters, filter...)
 	return rg
@@ -49,6 +54,7 @@ func NewRestGroup(serverConfig *web.ServerConfig, converter IConverter) *RestGro
 		port:         serverConfig.Port,
 		serverConfig: serverConfig,
 		converter:    converter,
+		host:         make([]string, 0),
 		filters:      make([]IFilter, 0),
 	}
 }

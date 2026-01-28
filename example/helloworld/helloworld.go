c36aea3af8b8a2e44e1ab8d7a06ced1e81faad39
tomcat <cooge123@gmail.com>
2026-01-28 12:23:29 +0800

    Update helloworld.go

--- example/helloworld/helloworld.go
+++ example/helloworld/helloworld.go
@@ -1,10 +1,16 @@
 package main
 
-import wf "github.com/chuccp/go-web-frame"
+import (
+	wf "github.com/chuccp/go-web-frame"
+	"github.com/chuccp/go-web-frame/web"
+)
 
 func main() {
 
 	webFrame := wf.NewWithAutoConfig()
+	webFrame.Get("/", func(c *web.Request) (any, error) {
+		return "hello world", nil
+	})
 	err := webFrame.Start()
 	if err != nil {
 		return

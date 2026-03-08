e353f91f0cde60e30c0b4f51a5ec3f92cae886cf
tomcat <cooge123@gmail.com>
2026-03-08 23:08:38 +0800

    Update model.go

--- model/model.go
+++ model/model.go
@@ -5,6 +5,7 @@ import (
 
 	"emperror.dev/errors"
 	"github.com/chuccp/go-web-frame/db"
+	"github.com/chuccp/go-web-frame/log"
 	"github.com/chuccp/go-web-frame/util"
 	"gorm.io/gorm"
 )
@@ -16,64 +17,70 @@ type Model[T any] struct {
 }
 
 func (a *Model[T]) IsExist() bool {
-	return a.db.Migrator().HasTable(a.tableName)
+	return a.getBb().Migrator().HasTable(a.tableName)
 }
 func (a *Model[T]) CreateTable() error {
 	if a.IsExist() {
 		return nil
 	}
 	t := util.NewPtr(a.entry)
-	return errors.WithStackIf(a.db.Table(a.tableName).AutoMigrate(t))
+	return errors.WithStackIf(a.getBb().Table(a.tableName).AutoMigrate(t))
 }
 func (a *Model[T]) DeleteTable() error {
 	t := util.NewPtr(a.entry)
-	err := a.db.Table(a.tableName).Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(t)
+	err := a.getBb().Table(a.tableName).Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(t)
 	return errors.WithStackIf(err)
 }
 func (a *Model[T]) GetTableName() string {
 	return a.tableName
 }
 func (a *Model[T]) Save(entry T) error {
-	return a.db.Table(a.tableName).Save(entry)
+	return a.getBb().Table(a.tableName).Save(entry)
+}
+func (a *Model[T]) getBb() *db.DB {
+	if a.db == nil {
+		log.Error("db is nil")
+	}
+	return a.db
 }
 
 func (a *Model[T]) Saves(entry []T) error {
-	return a.db.Table(a.tableName).Create(&entry)
+	return a.getBb().Table(a.tableName).Create(&entry)
 }
 
 func (a *Model[T]) SaveForMap(mapValue map[string]any) error {
-	return a.db.Table(a.tableName).Create(mapValue)
+	return a.getBb().Table(a.tableName).Create(mapValue)
 }
 
 func (a *Model[T]) SavesForMap(mapValues []map[string]any) error {
-	return a.db.Table(a.tableName).Create(mapValues)
+	return a.getBb().Table(a.tableName).Create(mapValues)
 }
 
 // SaveForMapWithPk saves a record from a map and returns the generated primary key
 func (a *Model[T]) SaveForMapWithPk(mapValue map[string]any, keyName string) (any, error) {
-	return a.db.Table(a.tableName).CreateMapWithPk(mapValue, keyName)
+	return a.getBb().Table(a.tableName).CreateMapWithPk(mapValue, keyName)
 }
 
 func (a *Model[T]) SaveForMapWithUintPk(mapValue map[string]any, keyName string) (uint, error) {
-	return a.db.Table(a.tableName).CreateMapWithUintPk(mapValue, keyName)
+	return a.getBb().Table(a.tableName).CreateMapWithUintPk(mapValue, keyName)
 }
 
 // CreateWithPk creates a record and returns the generated primary key
 func (a *Model[T]) CreateWithPk(entry T, keyName string, kind reflect.Kind) (any, error) {
-	return a.db.Table(a.tableName).CreateWithPk(entry, keyName, kind)
+	return a.getBb().Table(a.tableName).CreateWithPk(entry, keyName, kind)
 }
 
 func (a *Model[T]) Query() *Query[T] {
-	tx := a.db.Table(a.tableName)
+	tx := a.getBb().Table(a.tableName)
 	return &Query[T]{tx: tx, entry: a.entry}
 }
 
 func (a *Model[T]) Update() *Update[T] {
-	tx := a.db.Table(a.tableName)
+	tx := a.getBb().Table(a.tableName)
 	return &Update[T]{tx: tx, model: a.entry, wheres: NewUpdateWheres[T](tx)}
 }
 func (a *Model[T]) Delete() *Delete[T] {
-	tx := a.db.Table(a.tableName)
+	tx := a.getBb().Table(a.tableName)
 	return &Delete[T]{tx: tx, model: a.entry, wheres: NewDeleteWheres[T](tx, a.entry)}
 }
 

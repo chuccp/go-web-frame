44b81861ad0cfcef08202ec17596309d7877c9ba
tomcat <cooge123@gmail.com>
2026-04-08 23:57:24 +0800

    Update redis.go

--- redis/redis.go
+++ redis/redis.go
@@ -1,6 +1,8 @@
 package redis
 
 import (
+	"context"
+
 	config2 "github.com/chuccp/go-web-frame/config"
 	"github.com/redis/go-redis/v9"
 )
@@ -11,7 +13,7 @@ type Component struct {
 	client *redis.Client
 }
 
-func (l *Component) Init(config *config2.Config) error {
+func (l *Component) Init(ctx context.Context, config config2.IConfig) error {
 	var options = redis.Options{}
 	err := config.UnmarshalKey("web.redis", options)
 	if err != nil {

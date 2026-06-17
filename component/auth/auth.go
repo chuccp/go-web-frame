4ce0791d2f6c7f1d3f168cb031d28db509c937b7
tomcat <cooge123@gmail.com>
2026-06-17 20:46:31 +0800

    Update auth.go

--- component/auth/auth.go
+++ component/auth/auth.go
@@ -1,18 +1,15 @@
 package auth
 
 import (
-	"reflect"
-
-	"emperror.dev/errors"
 	"github.com/chuccp/go-web-frame/core"
 	"github.com/chuccp/go-web-frame/web"
 )
 
-type Authentication interface {
+type Authentication[U any] interface {
 	core.IService
 	SignIn(user any, request *web.Request) (any, error)
 	SignOut(request *web.Request) (any, error)
-	User(request *web.Request) (any, error)
+	User(request *web.Request) (U, error)
 }
 
 const (
@@ -32,12 +29,12 @@ func WithLogin() web.MetaOption {
 	return web.WithKey(LoginKey)
 }
 
-type AuthenticationFilter struct {
+type AuthenticationFilter[U any] struct {
 	ctx            *core.Context
-	authentication Authentication
+	authentication Authentication[U]
 }
 
-func (s *AuthenticationFilter) Init(ctx *core.Context) error {
+func (s *AuthenticationFilter[U]) Init(ctx *core.Context) error {
 	s.ctx = ctx
 	if s.authentication != nil {
 		err := s.authentication.Init(ctx)
@@ -47,17 +44,17 @@ func (s *AuthenticationFilter) Init(ctx *core.Context) error {
 	}
 	return nil
 }
-func (s *AuthenticationFilter) SignIn(user any, request *web.Request) (any, error) {
+func (s *AuthenticationFilter[U]) SignIn(user any, request *web.Request) (any, error) {
 	return s.authentication.SignIn(user, request)
 }
-func (s *AuthenticationFilter) SignOut(request *web.Request) (any, error) {
+func (s *AuthenticationFilter[U]) SignOut(request *web.Request) (any, error) {
 
 	return s.authentication.SignOut(request)
 }
-func (s *AuthenticationFilter) User(request *web.Request) (any, error) {
+func (s *AuthenticationFilter[U]) User(request *web.Request) (U, error) {
 	return s.authentication.User(request)
 }
-func (s *AuthenticationFilter) Handle(filterChain web.FilterChain, request *web.Request) (any, error) {
+func (s *AuthenticationFilter[U]) Handle(filterChain web.FilterChain, request *web.Request) (any, error) {
 	if request.HandlerMeta().Has(LoginKey) {
 		user, err := s.authentication.User(request)
 		if err != nil || user == nil {
@@ -67,22 +64,18 @@ func (s *AuthenticationFilter) Handle(filterChain web.FilterChain, request *web.
 	return filterChain.Next()
 }
 
-func User[T any](r *AuthenticationFilter, request *web.Request) (T, error) {
+func User[T any](r *AuthenticationFilter[T], request *web.Request) (T, error) {
 	u, err := r.User(request)
 	if err != nil {
 		var u T
 		return u, err
 	}
-	v, ok := u.(T)
-	if !ok {
-		return v, errors.New("Type conversion error. Please check if it is a pointer type." + reflect.TypeOf(u).Name())
-	}
-	return u.(T), nil
+	return u, nil
 
 }
 
-func NewAuthenticationFilter(authentication Authentication) *AuthenticationFilter {
-	return &AuthenticationFilter{
+func NewAuthenticationFilter[T any](authentication Authentication[T]) *AuthenticationFilter[T] {
+	return &AuthenticationFilter[T]{
 		authentication: authentication,
 	}
 }
